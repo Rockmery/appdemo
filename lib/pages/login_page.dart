e39562c7ac1b3072b1b855f72e85e1c5acc82114
rock mery <rockmery8888@gmail.com>
2022-03-30 15:48:06 +0530

    practices3

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Loginpage extends StatelessWidget {
+  final _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,38 +12,69 @@ class Loginpage extends StatelessWidget {
               horizontal: 16,
               vertical: 20,
             ),
-            child: Column(
-              children: <Widget>[
-                Image.asset(
-                  "assets/images/login_page.png",
-                  fit: BoxFit.cover,
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    Image.asset(
+                      "assets/images/login_page.png",
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Welcome",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Username ",
+                        hintText: "Enter Your Name",
+                      ),
+                      validator: (values) {
+                        if (values!.isEmpty) {
+                          return "Plese enter your user name";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 7,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Passwed",
+                        hintText: "Enter your Passwed",
+                      ),
+                      validator: (values) {
+                        if (values!.isEmpty) {
+                          return "plese enter Passwed";
+                        } else if (values.length < 6) {
+                          return "passwed lenth minimam is 6th";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      child: Text("Login"),
+                      onPressed: () {
+                        var isFormValidated =
+                            _formkey.currentState?.validate() ?? false;
+                      },
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "Welcome",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Username ",
-                    hintText: "Enter Your Name",
-                  ),
-                ),
-                SizedBox(
-                  height: 7,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Passwed",
-                    hintText: "Enter your Passwed",
-                  ),
-                )
-              ],
+              ),
             ),
           ),
         ),

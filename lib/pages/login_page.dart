3b409a6867a7712d399a3c716a299421c67b7792
rock mery <rockmery8888@gmail.com>
2022-03-30 10:59:41 +0530

    practices2

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,10 +4,48 @@ class Loginpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset("assets/images/2.png"),
-        ],
+      body: Container(
+        child: Material(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 20,
+            ),
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  "assets/images/login_page.png",
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Welcome",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Username ",
+                    hintText: "Enter Your Name",
+                  ),
+                ),
+                SizedBox(
+                  height: 7,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Passwed",
+                    hintText: "Enter your Passwed",
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
